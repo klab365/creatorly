637d160bb4bd399ede1b0314aa6509d5bf90457a
BuriKizilkaya <burak.kizilkaya@outlook.com>
2023-03-05 21:04:24 +0000

    Update Unit Test

--- src/infrastructure/yaml_configuration_loader.rs
+++ src/infrastructure/yaml_configuration_loader.rs
@@ -51,9 +51,7 @@ mod tests {
         ));
 
         // act
-        let template_specification = sut
-            .load_configuration("/workspaces/creatorly/src/infrastructure/test_data/test_template_spec.yml".to_string())
-            .unwrap();
+        let template_specification = sut.load_configuration("src/infrastructure/test_data/test_template_spec.yml".to_string()).unwrap();
 
         // assert
         for (index, template_item) in template_specification.questions.iter().enumerate() {
